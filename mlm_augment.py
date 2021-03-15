ea9684f12ecad74308fe9e9bdd0fdb96bda22c75
Saujas <saujas1999@gmail.com>
2021-03-15 16:21:20 +0530

    Pipeline for MLM augmentation

--- mlm_augment.py
+++ mlm_augment.py
@@ -1,10 +1,10 @@
-from transformers import AutoConfig, AutoModelForMaskedLM
-from transformers import AutoTokenizer
-# from transformers.data.data_collator import DataCollatorForWholeWordMask
-from torch.utils.data import Dataset, DataLoader
+import argparse
+import json
+
 import torch
 import torch.nn.functional as F
-from torch.distributions.categorical import Categorical
+from transformers import AutoConfig, AutoModelForMaskedLM
+from transformers import AutoTokenizer
 
 def simple_mask(lines, mask_token='[MASK]', masking_probability=0.15):
     tokenized = [line.split() for line in lines]
@@ -20,8 +20,9 @@ def simple_mask(lines, mask_token='[MASK]', masking_probability=0.15):
     
     return [' '.join(line) for line in tokenized]
 
-def augment(model, tokenizer, lines, n_augments=5, temperature=1, mask_token='[MASK]'):
+def augment(model, tokenizer, lines, n_augments=5, temperature=1, mask_token='[MASK]', device="cpu"):
     tokens = tokenizer(lines, return_tensors="pt", padding=True, truncation=True)
+    tokens.to(device)
     mask_token_id = tokenizer.convert_tokens_to_ids(mask_token)
     masked_indices = torch.where((tokens.input_ids == mask_token_id), torch.ones(tokens.input_ids.shape), torch.zeros(tokens.input_ids.shape)).bool()
 
@@ -36,3 +37,41 @@ def augment(model, tokenizer, lines, n_augments=5, temperature=1, mask_token='[M
         new_samples.append([tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(row, skip_special_tokens=True)) for row in augmented.detach().tolist()])
     
     return new_samples
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(add_help=True)
+    parser.add_argument("source_data", type=str)
+    parser.add_argument("dest_prefix", type=str)
+    parser.add_argument("n_augment", type=int)
+    parser.add_argument("pretrained_model_or_path", type=str)
+    parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument("--temperature", type=float, default=1)
+
+    args = parser.parse_args()
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    model = AutoModelForMaskedLM.from_pretrained(args.pretrained_model_or_path)
+    model.to(device)
+    tokenizer = AutoTokenizer.from_pretrained(args.pretrained_model_or_path)
+
+    with open(args.source_data, 'r') as f:
+        source_data = json.load(f)
+    
+    augmented_data = [list() for j in range(args.n_augment)]
+    i = 0
+    while i < len(source_data):
+        batch = [x["source"] for x in source_data[i:i + args.batch_size]]
+        masked_samples = simple_mask(batch)
+        augments = augment(model, tokenizer, masked_samples, n_augments=args.n_augment, temperature=args.temperature, device=device)
+        for j in range(args.n_augment):
+            augmented_data[j].extend(source_data[i:i + args.batch_size])
+            for k in range(i, i + len(batch)):
+                augmented_data[j][k]["source"] = augments[j][k - i]
+
+        i += args.batch_size
+        print("{} out of {} samples processed".format(i, len(source_data)))
+    
+    for j in range(args.n_augment):
+        with open("{}_{}.json".format(args.dest_prefix, j), 'w') as f:
+            json.dump(augmented_data[j], f)
