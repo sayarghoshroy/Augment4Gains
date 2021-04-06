73ff0686283ec4008341dabbec71e7238d6a5893
sayarghoshroy <sayar.1997@gmail.com>
2021-04-06 13:31:10 +0530

    Added evaluation sheet, updated sample runner script and results dump

--- src/classifier/sample_job.sh
+++ src/classifier/sample_job.sh
@@ -15,7 +15,7 @@ python run_experiment.py name tf_para_reddit_bal_2 path ../reddit test_mode 0 us
 
 python run_experiment.py name tf_para_reddit_no_bal path ../reddit test_mode 0 use_aug 1 aug_src para_aug.json model 1 bal 0
 python run_experiment.py name tf_para_reddit_bal_1 path ../reddit test_mode 0 use_aug 1 aug_src para_aug.json model 1 bal 1
-python run_experiment.py name tf_para_reddit_bal_2 path ../reddit test_mode 0 use_aug 1 aug_src para_aug.json model 1 bal 1
+python run_experiment.py name tf_para_reddit_bal_2 path ../reddit test_mode 0 use_aug 1 aug_src para_aug.json model 1 bal 2
 
 python run_experiment.py name reddit_no_bal path ../reddit test_mode 0 use_aug 0 model 0 bal 0
 python run_experiment.py name reddit_bal_1 path ../reddit test_mode 0 use_aug 0 model 0 bal 1
@@ -23,5 +23,5 @@ python run_experiment.py name reddit_bal_2 path ../reddit test_mode 0 use_aug 0 
 
 python run_experiment.py name reddit_no_bal path ../reddit test_mode 0 use_aug 0 model 1 bal 0
 python run_experiment.py name reddit_bal_1 path ../reddit test_mode 0 use_aug 0 model 1 bal 1
-python run_experiment.py name reddit_bal_2 path ../reddit test_mode 0 use_aug 0 model 1 bal 1
+python run_experiment.py name reddit_bal_2 path ../reddit test_mode 0 use_aug 0 model 1 bal 2
 
